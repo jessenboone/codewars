dd497f787e9e543b15dc40d89e4b0737f87e7266
jessenboone <jessenboone@gmail.com>
2017-06-30 10:33:46 -0600

    UFC problem

---
+++ 8-kyu/for-ufc-fans.js
@@ -0,0 +1,10 @@
+var quote = function(fighter) {
+console.log(fighter)
+  if (fighter.toLowerCase() === "conor mcgregor") {
+    return "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+  } else {
+    return "I am not impressed by your performance."
+  }
+};
+
+quote("George Saint Pierre")
