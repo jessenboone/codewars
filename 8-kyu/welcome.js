573982eb398de99fe1fdc50b0a1568439fc73327
jessenboone <jessenboone@gmail.com>
2017-06-30 09:11:07 -0600

    Welcome

---
+++ 8-kyu/welcome.js
@@ -0,0 +1,30 @@
+var language = {
+  english: 'Welcome',
+czech: 'Vitejte',
+danish: 'Velkomst',
+dutch: 'Welkom',
+estonian: 'Tere tulemast',
+finnish: 'Tervetuloa',
+flemish: 'Welgekomen',
+french: 'Bienvenue',
+german: 'Willkommen',
+irish: 'Failte',
+italian: 'Benvenuto',
+latvian: 'Gaidits',
+lithuanian: 'Laukiamas',
+polish: 'Witamy',
+spanish: 'Bienvenido',
+swedish: 'Valkommen',
+welsh: 'Croeso'
+}
+
+function greet(lang) {
+  for (var key in language) {
+    if (key == lang) {
+      return language[key];
+    }
+  }
+  return "Welcome";
+}
+
+greet("lithuanian")
