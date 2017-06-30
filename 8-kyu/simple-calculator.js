ff5a0821aacfda7a6301383af02b025279c64dc8
jessenboone <jessenboone@gmail.com>
2017-06-30 12:56:07 -0600

    calculator

---
+++ 8-kyu/simple-calculator.js
@@ -0,0 +1,15 @@
+function calculator(a, b, sign) {
+  if (sign === "+") {
+    return a + b;
+  } else if (sign === "-") {
+    return a - b;
+  } else if (sign === "*") {
+    return a * b;
+  } else if (sign === "/") {
+    return a / b;
+  } else {
+    return "unknown value"
+  }
+}
+
+calculator(2, 4, "+");
