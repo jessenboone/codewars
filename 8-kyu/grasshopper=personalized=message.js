00973380a25eb2109d6149b597c7bc85bc19da87
jessenboone <jessenboone@gmail.com>
2017-07-12 14:51:29 -0600

    opposite number

---
+++ 8-kyu/grasshopper=personalized=message.js
@@ -0,0 +1,7 @@
+function greet (name, owner) {
+  if (name === "boss" || name === "Boss") {
+    return "Hello boss"
+  }
+  return "Hello guest"
+}
+greet ('boss')
---
+++ 8-kyu/opposite-number.js
@@ -0,0 +1,5 @@
+function opposite(number) {
+  number = number - number - number;
+  return number
+}
+opposite(8)
