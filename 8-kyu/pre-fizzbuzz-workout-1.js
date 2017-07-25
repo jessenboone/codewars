8f5f41cf602d55426c909a68394696c1e9e90da5
jessenboone <jessenboone@gmail.com>
2017-07-25 14:18:43 -0600

    grasshopper object

---
+++ 8-kyu/pre-fizzbuzz-workout-1.js
@@ -0,0 +1,13 @@
+function preFizz(n) {
+  var arr = [];
+  var num = 1;
+  if (num === n) {
+    arr.push(num);
+    return arr;
+  } else if (num !== n) {
+    num = n + 1;
+    for (var j = 1; i < arr.length; i++) {
+      if (num ===)
+    }
+  }
+}
