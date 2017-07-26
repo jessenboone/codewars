5891548e98027a709f7e7d685168b50ea94dcc99
jessenboone <jessenboone@gmail.com>
2017-07-26 13:59:01 -0600

    grasshopper shopping list

---
+++ 8-kyu/grasshopper-messi-goals.js
@@ -0,0 +1,7 @@
+var laLigaGoals
+var championsLeagueGoals
+var copaDelReyGoals
+
+function totalGoals(laLigaGoals, championsLeagueGoals, copaDelReyGoals) {
+  return laLigaGoals + championsLeagueGoals + copaDelReyGoals;
+}
